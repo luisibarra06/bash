131c9453a796480410088fc3d733eba8b54154b3
luisibarra06 <totallynotabogusemail@gmail.com>
2019-02-04 08:55:33 -0800

    AA

---
+++ luisibarra06git.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  luisibarra06;
+git config --global user.name  luisibarra06;
+git config --global user.user luisibarra06;
+git config --global user.email totallynotabogusemail@gmail.com;
+git config --list;
